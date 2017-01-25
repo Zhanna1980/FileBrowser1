e0ca2007f4aa392aeee0fa5cb7c10e03fee46722
Zhanna <zhanna.libman@gmail.com>
2017-01-25 13:30:54 +0200

    save

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ const menu = [
 
 var shouldQuit = false;
 var currentFolderId = 0;
-var lastAddedId = 4;
+var lastAddedId = 6;
 var fsStorage = [];
 
 startApp();
@@ -39,9 +39,16 @@ function loadData() {
         // fill some initial data
         fsStorage = [
             {
-                id: 0, name: "root", children: [{id: 1, name: "sub1", children: [{id: 4, name: "file.txt"},]},
-                { id: 2, name: "sub2", children: []}, {id: 3, name: "file1.txt", content: "text"}
-            ]
+                id: 0, name: "root", children: [
+                    { id: 1, name: "sub1", children: [
+                        { id: 4, name: "file.txt"},
+                        { id: 5, name: "sub3", children: [
+                                {id: 6, name: "file2.txt", content: "content2"}
+                        ]}
+                    ]},
+                    { id: 2, name: "sub2", children: []},
+                    { id: 3, name: "file1.txt", content: "text"}
+                ]
             }
         ]
     }
@@ -64,15 +71,15 @@ function onMenuOptionSelected(selectedMenuOption) {
             break;
         //Create file or folder
         case 2:
-            // createFileOrFolder();
+            createFileOrFolder();
             break;
         //Delete file or folder
         case 3:
-            // deleteFileOrFolder();
+            deleteFileOrFolder();
             break;
         //Open file
         case 4:
-            // openFile();
+            openFile();
             break;
         //Quit Program
         case 5:
@@ -82,11 +89,26 @@ function onMenuOptionSelected(selectedMenuOption) {
             break;
     }
 }
-
-
+/**
+ * Finds current folder by id and prints it
+ * */
 function printCurrentFolder(currentFolderId){
-    const folderToPrint = findElementById(currentFolderId, fsStorage[0]);
+    const folderToPrint = findElementById(currentFolderId);
     printFolderContent(folderToPrint);
+}
+/**
+ * @return element object with given id
+ * */
+function findElementById(elementId) {
+    return findElementRec(elementId, fsStorage[0]).element;
+}
+
+/**
+ * @param elementId - the id of the child element.
+ * @return parent object.
+ * */
+function findParentByElementId(elementId) {
+    return findElementRec(elementId, fsStorage[0]).parent;
 }
 
 /**
@@ -97,7 +119,6 @@ function printFolderContent(folderToPrint){
     var folderContent = folderToPrint.children;
     //sorting by folder/file and alphabetically
     var sortedFolderContent = folderContent.sort(function(a,b){
-
         return (isFolder(a) == isFolder(b)) ? (a.name > b.name) : (isFolder(a) < isFolder(b)) });
     for (var i = 0; i < sortedFolderContent.length; i++) {
         console.log("\t" + (sortedFolderContent[i]).name);
@@ -123,57 +144,68 @@ function changeCurrentFolder() {
  * Changes current folder to one level up.
  * */
 function goUp() {
-    var parentId = (fsStorage[findElementById(currentFolderId)])[1];
-    currentFolderId = parentId;
-    printCurrentFolder(currentFolderId);
+    if (currentFolderId === 0) {
+        return;
+    }
+    const parentElement = findParentByElementId(currentFolderId);
+    currentFolderId = parentElement.id;
+    printFolderContent(parentElement);
 }
 
 /**
  * Changes current folder to a specified folder one level down.
  * */
 function goDown(folderName) {
-    const currentFolder = findElementById(currentFolderId, fsStorage[0]);
+    const folderToGo = findChildByName(currentFolderId, folderName);
+    if (folderToGo == null){
+        console.log("Not found");
+        return;
+    }
+    if (!isFolder(folderToGo)){
+        console.log(folderName, "is not a folder");
+    } else {
+        printFolderContent(folderToGo);
+        currentFolderId = folderToGo.id;
+    }
+}
+
+/**
+ * Searches for an element with given name among the children of the folder with given id
+ * @return child element with matching name
+ * */
+function findChildByName(folderId, childName){
+    const currentFolder = findElementById(folderId);
     for (var i = 0; i < currentFolder.children.length; i++){
-        if (currentFolder.children[i].name == folderName){
-            if (!isFolder(currentFolder.children[i])) {
-                console.log(folderName, "is not a folder");
-                return;
-            }
-            else{
-                printFolderContent(currentFolder.children[i]);
-                currentFolderId = currentFolder.children[i].id;
-                return;
-            }
+        if (currentFolder.children[i].name == childName){
+            return currentFolder.children[i];
         }
     }
-    console.log("Not found");
+    return null;
 }
 
 /**
  * Searches recursively for an element in fsStorage
  * @param folderId - integer which is stored in element.id
- * @param rootElement - object from which the function starts search
- * @return element with given id
+ * @param root - object from which the function starts search
+ * @return object with element with given id and with parent element.
  * */
-function findElementById(folderId, rootElement) {
-    if(rootElement["id"] === folderId){
-        return rootElement;
+function findElementRec(folderId, root) {
+    if(root.id === folderId){
+        return {element: root, parent: null};
     }
-    if (isFolder(rootElement)) {
-        var result = null;
-        for (var i = 0; i < rootElement["children"].length; i++) {
-            if (rootElement.children[i].id === folderId) {
-                return rootElement.children[i];
-            } else {
-                result = findElementById(folderId, rootElement.children[i]);
-                if (result != null) {
-                    return result;
-                }
+    var result = {element: null, parent: null};
+    if (isFolder(root)) {
+        for (var i = 0; i < root.children.length; i++) {
+            if (root.children[i].id === folderId){
+                return {element: root.children[i], parent: root};
+            }
+            result = findElementRec(folderId, root.children[i]);
+            if (result.element !== null && result.parent !== null) {
+                return result;
             }
         }
-        return result;
     }
-    return null;
+    return result;
 }
 
 /**
@@ -188,6 +220,81 @@ function isFolder (element){
     }
 }
 
+/**
+ * Creates new file or folder.
+ */
+function createFileOrFolder() {
+    const newFileOrFolderName = readlineSync.question("Please type file/folder name: ");
+    if (beenEnteredEmptyName(newFileOrFolderName)) {
+        return;
+    }
+    if (findChildByName(currentFolderId, newFileOrFolderName) != null) {
+        console.log("Such file/folder already exists.");
+        return;
+    }
+    const fileContent = readlineSync.question("Please write your content: ");
+    var newElement = {id: ++lastAddedId, name: newFileOrFolderName};
+    if (fileContent.length > 0){
+        newElement.content = fileContent;
+    }
+    else {
+        newElement.children = [];
+    }
+    const currentFolder = findElementById(currentFolderId);
+    currentFolder.children.push(newElement);
+    printFolderContent(currentFolder);
+}
+
+/**
+ * Deletes file or folder.
+ * */
+function deleteFileOrFolder() {
+    const nameToBeDeleted = readlineSync.question("Please type file/folder name to be deleted: ");
+    if (beenEnteredEmptyName(nameToBeDeleted)) {
+        return;
+    }
+    if (nameToBeDeleted == "root") {
+        console.log("This folder can not be deleted.");
+        return;
+    }
+    if (readlineSync.keyInYN("Are you sure?")) {
+        // 'Y' key was pressed.
+        const currentFolder = findElementById(currentFolderId);
+        var found = false;
+        for (var i = 0; i < currentFolder.children.length; i++){
+            if (currentFolder.children[i].name == nameToBeDeleted){
+                found = true;
+                currentFolder.children.splice(i, 1);
+                console.log(nameToBeDeleted, "was deleted.");
+                break;
+            }
+        }
+        if (!found) {
+            console.log("Not found");
+        }
+        printCurrentFolder(currentFolderId);
+    }
+}
+/**
+ * Displays file content.
+ */
+function openFile() {
+    const fileName = readlineSync.question("Which file to open? ");
+    if (beenEnteredEmptyName(fileName)) {
+        return;
+    }
+    const file = findChildByName(currentFolderId, fileName);
+    if ( file === null) {
+        console.log("Not found");
+        return;
+    }
+    if (!isFolder(file)) {
+        var hasContent = (file.content != null && file.content != undefined);
+        console.log(file.name, hasContent ? ("\n\t" + file.content) : "is empty");
+    } else {
+        console.log(fileName, "is not a file");
+    }
+}
 
 /**
  * Asks user for confirmation and quits the program
@@ -213,7 +320,7 @@ function quitProgram() {
  * @return true if the string empty or false otherwise.
  * */
 function beenEnteredEmptyName(name) {
-    if (name.length == 0) {
+    if (name.length === 0) {
         console.log("You didn't enter name.");
         return true;
     } else {
